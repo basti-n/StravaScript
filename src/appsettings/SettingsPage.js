0da5b872a72ac1713a1332b135e5119c943db299
Sebastian Neumair <sebastian.neumair@gmail.com>
2019-06-05 10:10:38 +0200

    Add minor adj. to DarkMode

--- src/appsettings/SettingsPage.js
+++ src/appsettings/SettingsPage.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
 import {
   StyledContainer,
   StyledMainHeadline,
