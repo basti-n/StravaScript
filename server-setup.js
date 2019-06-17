7461f52ef2a105959493af218b75d482a79e2f66
Sebastian Neumair <sebastian.neumair@gmail.com>
2019-06-17 20:48:29 +0200

    Add server config

--- server-setup.js
+++ server-setup.js
@@ -18,7 +18,7 @@ module.exports = function() {
     err ? console.log(err) : console.log('Server ready')
   })
 
-  app.get('/', function(req, res) {
+  app.get(['/', '/connect/*', 'goals/*'], function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 
